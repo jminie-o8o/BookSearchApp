b4088b36b316032578f08b5f6602cdb283d66e9e
hwj <jminie@naver.com>
2022-07-25 16:07:14 +0900

    feat: dependency 추가 및 카카오 책 검색 Response DTO 생성

---
+++ app/src/main/java/com/example/booksearchapp/data/model/SearchResponse.kt
@@ -0,0 +1,51 @@
+package com.example.booksearchapp.data.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SearchResponse(
+    @field:Json(name = "documents")
+    val documents: List<Document>,
+    @field:Json(name = "meta")
+    val meta: Meta
+)
+
+@JsonClass(generateAdapter = true)
+data class Meta(
+    @field:Json(name = "is_end")
+    val isEnd: Boolean,
+    @field:Json(name = "pageable_count")
+    val pageableCount: Int,
+    @field:Json(name = "total_count")
+    val totalCount: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class Document(
+    @field:Json(name = "authors")
+    val authors: List<String>,
+    @field:Json(name = "contents")
+    val contents: String,
+    @field:Json(name = "datetime")
+    val datetime: String,
+    @field:Json(name = "isbn")
+    val isbn: String,
+    @field:Json(name = "price")
+    val price: Int,
+    @field:Json(name = "publisher")
+    val publisher: String,
+    @field:Json(name = "sale_price")
+    val salePrice: Int,
+    @field:Json(name = "status")
+    val status: String,
+    @field:Json(name = "thumbnail")
+    val thumbnail: String,
+    @field:Json(name = "title")
+    val title: String,
+    @field:Json(name = "translators")
+    val translators: List<Any>,
+    @field:Json(name = "url")
+    val url: String
+)
