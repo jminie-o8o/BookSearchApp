33ed963fcfb0e48be611094438bf56631341a2d7
hwj <jminie@naver.com>
2022-07-26 14:25:55 +0900

    feat: SearchFragment와 BookSearchAdapter 연결

--- app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.booksearchapp.databinding.FragmentSearchBinding
+import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModelFactory
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+    private val bookSearchViewModel: BookSearchViewModel by activityViewModels { BookSearchViewModelFactory() }
+    private lateinit var bookSearchAdapter: BookSearchAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +26,21 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        bookSearchAdapter = BookSearchAdapter()
+        binding.rvSearchResult.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+            adapter = bookSearchAdapter
+        }
     }
 
     override fun onDestroyView() {
