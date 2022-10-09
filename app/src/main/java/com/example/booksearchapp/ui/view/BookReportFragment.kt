07d26a611ade70347ba0303fbaf2c0d7307eb450
hwj <jminie@naver.com>
2022-10-09 16:35:03 +0900

    feat: 하단 탭에 독후감 탭 추가

---
+++ app/src/main/java/com/example/booksearchapp/ui/view/BookReportFragment.kt
@@ -0,0 +1,25 @@
+package com.example.booksearchapp.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.booksearchapp.R
+import com.example.booksearchapp.databinding.FragmentBookBinding
+import com.example.booksearchapp.databinding.FragmentBookReportBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BookReportFragment : Fragment() {
+    private var _binding: FragmentBookReportBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBookReportBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
