f425e7c8cb7482ece2b13a1e643c0c26e906f993
hwj <jminie@naver.com>
2022-10-17 14:59:39 +0900

    feat: 앱 아이콘 추가

--- app/src/main/java/com/example/booksearchapp/util/Constants.kt
+++ app/src/main/java/com/example/booksearchapp/util/Constants.kt
@@ -5,7 +5,7 @@ import com.example.booksearchapp.BuildConfig
 object Constants {
     const val BASE_URL = "https://dapi.kakao.com/"
     const val API_KEY = BuildConfig.bookApiKey
-    const val SEARCH_BOOKS_TIME_DELAY = 100L
+    const val SEARCH_BOOKS_TIME_DELAY = 400L
     const val DATASTORE_NAME = "preferences_datastore"
     const val PAGING_SIZE = 15 // 페이징에서 한번에 가져올 데이터 수
 }
