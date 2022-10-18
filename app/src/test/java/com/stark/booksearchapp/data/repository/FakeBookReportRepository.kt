ac9a6b7c01c261b5027a1f69fd8786b4572c3710
hwj <jminie@naver.com>
2022-10-18 15:57:15 +0900

    feat: BookReportRegisterViewModel과 BookReportRepository를 테스트하는 통합 테스트 수행

---
+++ app/src/test/java/com/stark/booksearchapp/data/repository/FakeBookReportRepository.kt
@@ -0,0 +1,32 @@
+package com.stark.booksearchapp.data.repository
+
+import androidx.paging.PagingData
+import com.stark.booksearchapp.data.model.BookReport
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+// 테스트에는 사용할 수 있지만 프로덕션에는 사용할 수 없는 Fake-Test-Double
+class FakeBookReportRepository : BookReportRepository {
+
+    private val reportItems = mutableListOf<BookReport>()
+
+    override suspend fun getBookReportDetail(isbn: String): BookReport {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertBookReport(bookReport: BookReport) {
+        reportItems.add(bookReport)
+    }
+
+    override suspend fun deleteBookReport(bookReport: BookReport) {
+        reportItems.remove(bookReport)
+    }
+
+    override fun getBookReport(): Flow<List<BookReport>> {
+        return flowOf(reportItems)
+    }
+
+    override fun getBookReportPaging(): Flow<PagingData<BookReport>> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/test/java/com/stark/booksearchapp/ui/viewmodel/BookReportRegisterViewModelTest.kt
@@ -0,0 +1,48 @@
+package com.stark.booksearchapp.ui.viewmodel
+
+import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
+import com.stark.booksearchapp.data.model.BookReport
+import com.stark.booksearchapp.data.repository.FakeBookReportRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@MediumTest
+class BookReportRegisterViewModelTest {
+
+    private lateinit var viewModel: BookReportRegisterViewModel
+
+    // 프로덕션 코드에서는 ViewModel 을 싱글톤으로 만들어야하지만
+    // 테스트 코드에서는 그럴 필요가 없다.
+    @Before
+    fun setUp() {
+        viewModel = BookReportRegisterViewModel(FakeBookReportRepository())
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun save_book_report_test() = runTest {
+        val saveBookReport = BookReport("a", "b", "c", "d",
+            "e", "f", "g", "h")
+
+        viewModel.saveBookReport(saveBookReport)
+
+        val getBookReport = viewModel.bookReportsForTest.first()
+        assertThat(getBookReport).contains(saveBookReport)
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun delete_book_report_test() = runTest {
+        val saveBookReport = BookReport("a", "b", "c", "d",
+            "e", "f", "g", "h")
+
+        viewModel.saveBookReport(saveBookReport)
+
+        val getBookReport = viewModel.bookReportsForTest.first()
+        assertThat(getBookReport).contains(saveBookReport)
+    }
+}
