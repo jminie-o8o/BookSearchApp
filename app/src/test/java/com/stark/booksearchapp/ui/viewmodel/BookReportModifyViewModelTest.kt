942a30a5321d1a9fd47718c2ec723ef9a5afbedc
hwj <jminie@naver.com>
2022-10-18 15:59:55 +0900

    feat: BookReportModifyViewModel과 BookReportRepository를 테스트하는 통합 테스트 수행

--- app/src/test/java/com/stark/booksearchapp/ui/viewmodel/BookReportModifyViewModelTest.kt
+++ app/src/test/java/com/stark/booksearchapp/ui/viewmodel/BookReportModifyViewModelTest.kt
@@ -1,5 +1,34 @@
 package com.stark.booksearchapp.ui.viewmodel
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth
+import com.stark.booksearchapp.data.model.BookReport
+import com.stark.booksearchapp.data.repository.FakeBookReportRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
 
-class BookReportModifyViewModelTest
+class BookReportModifyViewModelTest {
+
+    private lateinit var viewModel: BookReportModifyViewModel
+
+    // 프로덕션 코드에서는 ViewModel 을 싱글톤으로 만들어야하지만
+    // 테스트 코드에서는 그럴 필요가 없다.
+    @Before
+    fun setUp() {
+        viewModel = BookReportModifyViewModel(FakeBookReportRepository())
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun save_book_report_test() = runTest {
+        val saveBookReport = BookReport("a", "b", "c", "d",
+            "e", "f", "g", "h")
+
+        viewModel.saveBookReport(saveBookReport)
+
+        val getBookReport = viewModel.bookReportsForTest.first()
+        Truth.assertThat(getBookReport).contains(saveBookReport)
+    }
+}
