739f5fddc9a51523b8d9f7c582136ea1a26cf23b
hwj <jminie@naver.com>
2022-11-03 14:54:54 +0900

    feat: AlarmReceiver(BroadcastReceiver) 구현

---
+++ app/src/main/java/com/stark/booksearchapp/util/AlarmReceiver.kt
@@ -0,0 +1,55 @@
+package com.stark.booksearchapp.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.stark.booksearchapp.R
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "1000"
+        const val NOTIFICATION_ID = 100
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        // 채널 생성
+        createNotificationChannel(context)
+        // 알림
+        notifyNotification(context)
+
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        // context : 실행하고 있는 앱의 상태나 맥락을 담고 있음
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "하루독후감 알람",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            NotificationManagerCompat.from(context)
+                .createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun notifyNotification(context: Context) {
+        with(NotificationManagerCompat.from(context)) {
+            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("하루독후감")
+                .setContentText("독후감 쓸 시간이에요:)")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+
+            notify(NOTIFICATION_ID, build.build())
+        }
+    }
+
+}
