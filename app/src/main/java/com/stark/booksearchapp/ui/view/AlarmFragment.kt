1ac6b4fa0f651f2ae0cc3038c2ff3d614471083d
hwj <jminie@naver.com>
2022-11-03 15:13:35 +0900

    feat: AlarmFragment UI 구현

---
+++ app/src/main/java/com/stark/booksearchapp/ui/view/AlarmFragment.kt
@@ -0,0 +1,35 @@
+package com.stark.booksearchapp.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stark.booksearchapp.R
+import com.stark.booksearchapp.databinding.FragmentAlarmBinding
+import com.stark.booksearchapp.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AlarmFragment : Fragment() {
+
+    private var _binding: FragmentAlarmBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAlarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
