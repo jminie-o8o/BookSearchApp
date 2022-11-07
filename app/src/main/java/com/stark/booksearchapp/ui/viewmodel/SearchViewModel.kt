498281ca6af0b5a47f5061b2cddad31f41ed4a6b
hwj <jminie@naver.com>
2022-11-07 17:23:35 +0900

    feat: SearchViewModel에 Coroutine Exception Handler로 예외 처리 구현

--- app/src/main/java/com/stark/booksearchapp/ui/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/stark/booksearchapp/ui/viewmodel/SearchViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.stark.booksearchapp.data.model.Book
+import com.stark.booksearchapp.data.model.CEHModel
 import com.stark.booksearchapp.data.repository.BookSearchRepository
+import com.stark.booksearchapp.util.CoroutineException
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
@@ -31,14 +35,23 @@ class SearchViewModel @Inject constructor(
     private val _searchPagingResult = MutableStateFlow<PagingData<Book>>(PagingData.empty())
     val searchPagingResult: StateFlow<PagingData<Book>> = _searchPagingResult.asStateFlow()
 
-    fun handleSearchWord(word: String) {
+    private val _error = MutableSharedFlow<CEHModel>()
+    val error: SharedFlow<CEHModel> = _error
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         viewModelScope.launch {
+            _error.emit(CoroutineException.checkThrowable(throwable))
+        }
+    }
+
+    fun handleSearchWord(word: String) {
+        viewModelScope.launch(exceptionHandler) {
             _searchWord.emit(word)
         }
     }
 
     fun searchBooksPaging(query: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             bookSearchRepository.searchBooksPaging(query, getSortMode())
                 .cachedIn(viewModelScope)
                 .collect {
