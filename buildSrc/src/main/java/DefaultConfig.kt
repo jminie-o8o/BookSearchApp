1ea9659a470e7b2f833fab889ed06ae84d837e30
hwj <jminie@naver.com>
2022-11-10 13:30:39 +0900

    docs: 앱 업데이트를 위해 DefaultConfig의 버전코드, 버전네임 변경

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -2,6 +2,6 @@ object DefaultConfig {
     const val COMPILE_SDK_VERSION = 33
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 32
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "1.1"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "1.2"
 }
